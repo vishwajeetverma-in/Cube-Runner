1af4bd4d1aed61790c664acab87748b67f474e01
vishwajeetverma-in <vishwa11126@gmail.com>
2024-01-05 13:24:30 +0530

    / when mouse is clicked once jump is fine but when mouse is clicked again (when player is still in the air) it again starts jumping from air to more height. //  we want that it's next jump should be when it comes down on the ground.For this // execution sequence | Awake - Start - (*Physics* FixedUpdate - OnTrigger-OnCollision) - (*Input events* OnMouseDown) - Update - LateUpdate -(*The last* OnDestroy) // use a temp bool variable "canJump" can to achieve this . when mouses is clicked and canbool is true "then only" force will be applied. the truth value of this variable // is controlled inside methods OnCollisionEnter (when player is in contact with ground (colliders are touching each other)) in this case assign "canJump = true" //                              OnCollisionExit (when player isn't in contact with ground (colliders are not touching each other)) in this case assign "canJump = false" // As collison runs before Update() so bool value can be changed according to collison and then condition will be applid on force.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -22,11 +22,33 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && canJump)
         {
             rb.AddForce(Vector3.up * jumpForce , ForceMode.Impulse);
             
         }
             
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.gameObject.tag == "Ground")
+        {
+            canJump = true;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.gameObject.tag == "Ground")
+        {
+            canJump = false;
+        }
+    }
 }
+// when mouse is clicked once jump is fine but when mouse is clicked again (when player is still in the air) it again starts jumping from air to more height.
+//  we want that it's next jump should be when it comes down on the ground.For this 
+// execution sequence | Awake - Start - (*Physics* FixedUpdate - OnTrigger-OnCollision) - (*Input events* OnMouseDown) - Update - LateUpdate -(*The last* OnDestroy)
+// use a temp bool variable "canJump" can to achieve this . when mouses is clicked and canbool is true "then only" force will be applied. the truth value of this variable 
+// is controlled inside methods OnCollisionEnter (when player is in contact with ground (colliders are touching each other)) in this case assign "canJump = true"
+//                              OnCollisionExit (when player isn't in contact with ground (colliders are not touching each other)) in this case assign "canJump = false"
+// As collison runs before Update() so bool value can be changed according to collison and then condition will be applid on force. 
