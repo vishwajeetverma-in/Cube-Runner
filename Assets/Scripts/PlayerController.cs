19d9ce0af3240a4c514d13c2889a0573c67a4562
vishwajeetverma-in <vishwa11126@gmail.com>
2024-01-05 14:13:02 +0530

    obstacle coming towards player | Player has to jump | detect the trigger type collision and reload the scene | make sure isTrigger box checked of obstacle gameobject  if OnTrigger method is inside Playerscript|

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -42,6 +43,15 @@ public class PlayerController : MonoBehaviour
         if (collision.collider.gameObject.tag == "Ground")
         {
             canJump = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+       
+        if(other.gameObject.tag== "Obstacle")
+        {
+            SceneManager.LoadScene("Level1");
         }
     }
 }
